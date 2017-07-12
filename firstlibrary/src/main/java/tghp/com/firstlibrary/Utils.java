7d3174c5667c6a868ab2c0735aa07f9b601645a1
Monkeynessss <695678923@qq.com>
2017-07-12 15:13:40 +0800

    Initial commit

---
+++ firstlibrary/src/main/java/tghp/com/firstlibrary/Utils.java
@@ -0,0 +1,17 @@
+package tghp.com.firstlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by 69567 on 2017/7/12.
+ *
+ */
+
+public class Utils {
+
+    public static void toast(Context context) {
+        Toast.makeText(context, "library", Toast.LENGTH_SHORT).show();
+    }
+
+}
